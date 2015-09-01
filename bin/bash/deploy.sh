c9ffbd9ef1cf0196afa072aa78cb70d59e074b17
Bruno Leite Alves <brunoleitealves@gmail.com>
2015-09-01 01:45:03 -0300

    refactors build to use only one package.json and removes dependecies from ansible scripts

--- bin/bash/deploy.sh
+++ bin/bash/deploy.sh
@@ -2,12 +2,8 @@
 
 set -o errexit # Exit on error
 
-npm install -g jspm@beta
-cd client
-npm install
-jspm config registries.github.remote https://github.jspm.io
-jspm config registries.github.auth JSPM_GITHUB_AUTH_TOKEN
-jspm config registries.github.maxRepoSize 0
-jspm config registries.github.handler jspm-github
-jspm install -y
-cd ..
+node_modules/.bin/jspm config registries.github.remote https://github.jspm.io
+node_modules/.bin/jspm config registries.github.auth $JSPM_GITHUB_AUTH_TOKEN
+node_modules/.bin/jspm config registries.github.maxRepoSize 0
+node_modules/.bin/jspm config registries.github.handler jspm-github
+node_modules/.bin/jspm install -y

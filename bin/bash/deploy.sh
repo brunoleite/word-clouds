bee8a1fcd5d1f6e13a377210b9eee361c40cc094
Bruno Leite Alves <brunoleitealves@gmail.com>
2015-09-09 09:56:26 -0300

    Removes unnecessary bash script

--- bin/bash/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -o errexit # Exit on error
-
-node_modules/.bin/jspm config registries.github.remote https://github.jspm.io
-node_modules/.bin/jspm config registries.github.auth $JSPM_GITHUB_AUTH_TOKEN
-node_modules/.bin/jspm config registries.github.maxRepoSize 0
-node_modules/.bin/jspm config registries.github.handler jspm-github
-node_modules/.bin/jspm install -y

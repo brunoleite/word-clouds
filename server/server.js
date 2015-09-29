04ce832fc6f80479ad4f2ef48aa703229cf3bae4
Bruno Leite Alves <brunoleitealves@gmail.com>
2015-09-29 01:11:05 -0300

    Adds function to count words

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+var bodyParser = require('body-parser')
+var wordClouds = require('../lib');
 
 app.use(express.static('client'));
+app.use(bodyParser.json());
+
+app.post('/word-clouds', function(request, response) {
+	var text = request.body.englishText;
+	response.send(wordClouds(text));
+});
 
 var server = app.listen(port, function () {
   var host = server.address().address;
--- test/index.js
+++ test/index.js
@@ -1,10 +1,18 @@
 'use strict';
 
-var assert = require('assert');
 var wordClouds = require('../lib');
+var expect = require('chai').expect;
 
 describe('word-clouds', function () {
-  it('first test!', function () {
-    assert(true);
+  it('counts word', function () {
+    var sentence = 'Sentence';
+    var temp = wordClouds(sentence);
+    expect(temp).to.deep.include.members([{ word: 'Sentence', size: 1 }]);
+  });
+
+  it('counts words', function () {
+    var sentence = 'This is a sentence';
+    var temp = wordClouds(sentence);
+    expect(temp).to.deep.include.members([{ word: 'This', size: 1 }, { word: 'is', size: 1 }, { word: 'a', size: 1 }, { word: 'sentence', size: 1 }]);
   });
 });

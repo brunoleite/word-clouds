ddba67ec6d1ae9d3610937655d2bc234cff9cdae
Bruno Leite Alves <brunoleitealves@gmail.com>
2015-09-29 12:36:53 -0300

    Writing more tests to count words

--- test/index.js
+++ test/index.js
@@ -3,16 +3,44 @@
 var wordClouds = require('../lib');
 var expect = require('chai').expect;
 
-describe('word-clouds', function () {
-  it('counts word', function () {
+describe('it counts', function () {
+  it('single word', function () {
     var sentence = 'Sentence';
     var temp = wordClouds(sentence);
     expect(temp).to.deep.include.members([{ word: 'Sentence', size: 1 }]);
   });
 
-  it('counts words', function () {
+  it('multiple words', function () {
     var sentence = 'This is a sentence';
     var temp = wordClouds(sentence);
-    expect(temp).to.deep.include.members([{ word: 'This', size: 1 }, { word: 'is', size: 1 }, { word: 'a', size: 1 }, { word: 'sentence', size: 1 }]);
+    expect(temp).to.deep.include.members([
+    	{ word: 'This', size: 1 }, 
+    	{ word: 'is', size: 1 }, 
+    	{ word: 'a', size: 1 }, 
+    	{ word: 'sentence', size: 1 }
+    ]);
   });
+
+  it('words with marks', function () {
+    var sentence = 'This! is, a? sentence...';
+    var temp = wordClouds(sentence);
+    expect(temp).to.deep.include.members([
+    	{ word: 'This', size: 1 }, 
+    	{ word: 'is', size: 1 }, 
+    	{ word: 'a', size: 1 }, 
+    	{ word: 'sentence', size: 1 }
+    ]);
+  });
+
+  it('capitlized words', function () {
+    var sentence = 'This Is A Sentence';
+    var temp = wordClouds(sentence);
+    expect(temp).to.deep.include.members([
+    	{ word: 'This', size: 1 }, 
+    	{ word: 'Is', size: 1 }, 
+    	{ word: 'A', size: 1 }, 
+    	{ word: 'Sentence', size: 1 }
+    ]);
+  });
+
 });

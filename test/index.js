193a27f03128855be17f0647f5047939a0ca6756
Bruno Leite Alves <brunoleitealves@gmail.com>
2015-10-05 20:41:39 -0300

    Adds filter for stop words

--- test/index.js
+++ test/index.js
@@ -14,9 +14,6 @@ describe('it counts', function () {
     var sentence = 'This is a sentence';
     var temp = wordClouds(sentence);
     expect(temp).to.deep.include.members([
-    	{ text: 'THIS', size: 1 }, 
-    	{ text: 'IS', size: 1 }, 
-    	{ text: 'A', size: 1 }, 
     	{ text: 'SENTENCE', size: 1 }
     ]);
   });
@@ -25,9 +22,6 @@ describe('it counts', function () {
     var sentence = 'This! is, a? sentence...';
     var temp = wordClouds(sentence);
     expect(temp).to.deep.include.members([
-    	{ text: 'THIS', size: 1 }, 
-    	{ text: 'IS', size: 1 }, 
-    	{ text: 'A', size: 1 }, 
     	{ text: 'SENTENCE', size: 1 }
     ]);
   });
@@ -36,9 +30,6 @@ describe('it counts', function () {
     var sentence = 'This Is A Sentence';
     var temp = wordClouds(sentence);
     expect(temp).to.deep.include.members([
-    	{ text: 'THIS', size: 1 }, 
-    	{ text: 'IS', size: 1 }, 
-    	{ text: 'A', size: 1 }, 
     	{ text: 'SENTENCE', size: 1 }
     ]);
   });
@@ -47,10 +38,19 @@ describe('it counts', function () {
     var sentence = 'This Is A SENtence';
     var temp = wordClouds(sentence);
     expect(temp).to.deep.include.members([
-    	{ text: 'THIS', size: 1 }, 
-    	{ text: 'IS', size: 1 }, 
-    	{ text: 'A', size: 1 }, 
     	{ text: 'SENTENCE', size: 1 }
+    ]);
+  });
+
+  it('words without stop words', function () {
+    var sentence = 'I am counting this sentence, word by word, without stop words.';
+    var temp = wordClouds(sentence);
+    expect(temp).to.deep.include.members([
+    	{ text: 'COUNTING', size: 1 }, 
+    	{ text: 'SENTENCE', size: 1 }, 
+    	{ text: 'STOP', size: 1 }, 
+    	{ text: 'WORD', size: 2 }, 
+    	{ text: 'WORDS', size: 1 }
     ]);
   });
 
